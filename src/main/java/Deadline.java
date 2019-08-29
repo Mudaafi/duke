987d64935bee897f4181cb5a50a5c01be41b488c
Ahmad Mudaafi <SleepyUndergrad@gmail.com>
2019-08-29 23:10:16 +0800

    Completed: Level 8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,26 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Deadline extends Task {
+    protected Date datetime;
     // Initialization
     public Deadline(String name) {
 	super(name);
 	this.type = "D";
 	this.recordTaskDetails(name);
+	this.parseDateTime();
+    }
+
+    private void parseDateTime() {
+	SimpleDateFormat formatx = new SimpleDateFormat("dd/mm/yyyy HHmm");
+	if (this.detailDesc.equals("by")) {
+	    try {
+	        this.datetime = formatx.parse(this.taskDetails);
+		System.out.println("Date Interpreted: " + formatx.format(this.datetime));
+	    } catch (Exception e) {
+		System.out.println("Invalid Input. Unable to interpret Datetime");
+		this.datetime = new Date();
+	    }
+	}
     }
 }
